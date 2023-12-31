1c853f9f37267dc21cdaa5b275dcb3837be23645
RameshBabuRb <150510357+RameshBabuRb@users.noreply.github.com>
2023-12-31 19:15:17 +0530

    Update server.js

--- back-end/server.js
+++ back-end/server.js
@@ -6,7 +6,7 @@ const nodemailer = require("nodemailer");
 require("dotenv").config();
 
 const app = express();
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
   orgin:["https://ramesh-raja-rani-frontend.vercel.app"],
   method:["POST","GET"],
